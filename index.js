5808c3b6c6d1d6ec22857b87566b18dc56a64416
clement <maboiteaspam@wanadoo.fr>
2015-05-16 18:01:56 +0800

    Publish same 1.0.1

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
 var fs = require('fs');
 var path = require('path');
 var program = require('commander');
+var Config = require("project-bin-config");
 var Cluc = require('cluc');
 var Transport = Cluc.transports.process;
+
+
+//region Program
 
 var projectPath = process.cwd();
 var pkg = require(path.join(projectPath, 'package.json') );
@@ -14,7 +18,125 @@ var newRevision;
 var releaseType;
 var sshUrl = pkg.repository.url.replace(/https?:\/\//,'ssh://git@');
 
+program
+  .version(require(path.join(__dirname, 'package.json') ).version)
+  .usage('[env]')
+  .description('Publish your project')
+  .arguments('[env]')
+  .parse(process.argv);
+
+
+var env = !program.env?'local':program.env;
+
+(new Config()).load().get(env)
+  .forEach(function(machine){
+
+    var line = new Cluc()
+
+      .ensureFileContains('.gitignore', '\n.local\n')
+      .ensureFileContains('.gitignore', '\n.idea/\n')
+
+      .then(function(next){
+        if(!pkg.name){
+          throw 'pkg.name is missing';
+        }
+        if(!pkg.repository){
+          throw 'pkg.repository is missing';
+        }
+        this.saveValue('pkgName', pkg.name);
+        this.saveValue('pkgRepository', pkg.repository);
+        this.saveValue('sshUrl', sshUrl);
+        this.saveValue('projectPath', projectPath);
+        this.saveValue('branch', machine.profileData.publish.branch || 'master');
+        this.saveValue('gitAuth', machine.profileData.github);
+        next();
+
+      }).choose('Select a revision type', releaseTypes, function(release){
+        releaseType = release.match(/^\s*([a-z]+)\s*=>\s*(.+)$/i)[1];
+        newRevision = release.match(/^\s*([a-z]+)\s*=>\s*(.+)$/i)[2];
+        this.saveValue('releaseType', releaseType);
+        this.saveValue('newRevision', newRevision);
+
+      }).title('', 'Release project\non <%=branch%> to <%=releaseType%> <%=newRevision%>')
+      .stream('cd <%=projectPath%>', function(){
+        this.display();
+        this.dieOnError();
+
+      }).stream('git status', function(){
+        this.must(/(est propre|is clean)/ig, 'Tree should be clean')
+          .or(line.confirmToStop('Tree is unclean, stop now ?', true));
+
+      }).stream('git fetch <%=sshUrl%>', function(){
+        sendGhAuth(this);
+        this.display();
+
+      }).stream('git checkout <%=branch%>', function(){
+        sendGhAuth(this);
+        this.display();
+
+      }).then(function(next){
+        pkg.version = this.getValue('newRevision');
+        var releaseType = this.getValue('releaseType');
+        if( releaseType!=='same'){
+          fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2)+'\n');
+          fs.writeFileSync('./version', releaseType+' '+pkg.version+'\n');
+        }
+        next();
+
+      }).stream('git pull <%=sshUrl%> <%=branch%>', function(){
+        sendGhAuth(this);
+        this.display();
+
+      }).stream('git add -A', function(){
+        sendGhAuth(this);
+        this.display();
+
+      }).stream('git commit -am "Publish <%=releaseType%> <%=newRevision%>"', function(){
+        this.success(/\[([\w-]+)\s+([\w-]+)]/i,
+          'branch\t\t%s\nnew revision\t%s');
+        this.success(/([0-9]+)\s+file[^0-9]+?([0-9]+)?[^0-9]+?([0-9]+)?/i,
+          'changed\t%s\nnew\t\t%s\ndeleted\t%s');
+        this.warn(/(est propre|is clean)/i, 'Nothing to do');
+        sendGhAuth(this);
+        this.display();
+
+      }).stream('git -c core.askpass=true push <%= sshUrl %> <%= branch %>', function(){
+        this.warn(/fatal:/);
+        this.success(/(:<remoteRev>[\w-]+)[.]+(:<localRev>[\w-]+)\s+(:<remoteBranch>[\w-]+)\s+->\s+(:<localBranch>[\w-]+)/,
+          'pushed\nlocal\tlocalBranch@localRev\nremote\tremoteBranch@remoteRev');
+        this.success('Everything up-to-date');
+        sendGhAuth(this);
+        this.display();
+
+      }).skip(pkg.private).stream('npm publish', function(){
+        this.warn(/fatal:/);
+        this.success(/(:<remoteRev>[\w-]+)[.]+(:<localRev>[\w-]+)\s+(:<remoteBranch>[\w-]+)\s+->\s+(:<localBranch>[\w-]+)/,
+          'pushed\nlocal\tlocalBranch@localRev\nremote\tremoteBranch@remoteRev');
+        this.success('Everything up-to-date');
+        sendGhAuth(this);
+        this.display();
+
+      }).skip(!machine.profileData.github).then(function(then){
+        var tagname = this.getValue('newRevision');
+        var releaseType = this.getValue('releaseType');
+        var branch = this.getValue('branch');
+        gitHubRelease(this, branch, pkg.name, tagname, releaseType, then);
+
+      }).title('', '\nAll done !\n\n' +
+      'Published <%=pkgName%>\n' +
+      'on <%=branch%> to <%=releaseType%> <%=newRevision%>\n')
+
+      .run(new Transport());
+
+  });
+
+//endregion
+
+
+
+
 //region Git auth and github release
+
 function sendGhAuth(context){
   var gitAuth = context.getValue('gitAuth');
   context.answer(/^Username/i, gitAuth.username);
@@ -46,10 +168,14 @@ function gitHubRelease(context, branch, reponame, tagname, releaseType, then){
     prerelease: releaseType==='prerelease'
   }, then);
 }
+
 //endregion
 
 
+
+
 //region release helper
+
 function listReleaseTypes(currentRevision){
 
   var semver = require('semver');
@@ -70,125 +196,5 @@ function listReleaseTypes(currentRevision){
   });
   return releaseTypes;
 }
+
 //endregion
-
-
-program
-  .version(require(path.join(__dirname, 'package.json') ).version)
-
-program.on('--help', function(){
-  program.outputHelp();
-  process.exit();
-});
-
-program
-  .command('[env]')
-  .description('Publish your project')
-  .action(function (env) {
-
-    env = !env?'local':env;
-
-    (new Config()).load().get(env)
-      .forEach(function(machine){
-
-        var line = new Cluc()
-
-          .ensureFileContains('.gitignore', '\n.local\n')
-          .ensureFileContains('.gitignore', '\n.idea/\n')
-
-          .then(function(next){
-            if(!pkg.name){
-              throw 'pkg.name is missing';
-            }
-            if(!pkg.repository){
-              throw 'pkg.repository is missing';
-            }
-            this.saveValue('pkgName', pkg.name);
-            this.saveValue('pkgRepository', pkg.repository);
-            this.saveValue('sshUrl', sshUrl);
-            this.saveValue('projectPath', projectPath);
-            this.saveValue('branch', machine.profileData.publish.branch || 'master');
-            this.saveValue('gitAuth', machine.profileData.github);
-            next();
-
-          }).choose('Select a revision type', releaseTypes, function(release){
-            releaseType = release.match(/^\s*([a-z]+)\s*=>\s*(.+)$/i)[1];
-            newRevision = release.match(/^\s*([a-z]+)\s*=>\s*(.+)$/i)[2];
-            this.saveValue('releaseType', releaseType);
-            this.saveValue('newRevision', newRevision);
-
-          }).title('', 'Release project\non <%=branch%> to <%=releaseType%> <%=newRevision%>')
-          .stream('cd <%=projectPath%>', function(){
-            this.display();
-            this.dieOnError();
-
-          }).stream('git status', function(){
-            this.must(/(est propre|is clean)/ig, 'Tree should be clean')
-              .or(line.confirmToStop('Tree is unclean, stop now ?', true));
-
-          }).stream('git fetch <%=sshUrl%>', function(){
-            sendGhAuth(this);
-            this.display();
-
-          }).stream('git checkout <%=branch%>', function(){
-            sendGhAuth(this);
-            this.display();
-
-          }).then(function(next){
-            pkg.version = this.getValue('newRevision');
-            var releaseType = this.getValue('releaseType');
-            if( releaseType!=='same'){
-              fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2)+'\n');
-              fs.writeFileSync('./version', releaseType+' '+pkg.version+'\n');
-            }
-            next();
-
-          }).stream('git pull <%=sshUrl%> <%=branch%>', function(){
-            sendGhAuth(this);
-            this.display();
-
-          }).stream('git add -A', function(){
-            sendGhAuth(this);
-            this.display();
-
-          }).stream('git commit -am "Publish <%=releaseType%> <%=newRevision%>"', function(){
-            this.success(/\[([\w-]+)\s+([\w-]+)]/i,
-              'branch\t\t%s\nnew revision\t%s');
-            this.success(/([0-9]+)\s+file[^0-9]+?([0-9]+)?[^0-9]+?([0-9]+)?/i,
-              'changed\t%s\nnew\t\t%s\ndeleted\t%s');
-            this.warn(/(est propre|is clean)/i, 'Nothing to do');
-            sendGhAuth(this);
-            this.display();
-
-          }).stream('git -c core.askpass=true push <%= sshUrl %> <%= branch %>', function(){
-            this.warn(/fatal:/);
-            this.success(/(:<remoteRev>[\w-]+)[.]+(:<localRev>[\w-]+)\s+(:<remoteBranch>[\w-]+)\s+->\s+(:<localBranch>[\w-]+)/,
-              'pushed\nlocal\tlocalBranch@localRev\nremote\tremoteBranch@remoteRev');
-            this.success('Everything up-to-date');
-            sendGhAuth(this);
-            this.display();
-
-          }).skip(pkg.private).stream('npm publish', function(){
-            this.warn(/fatal:/);
-            this.success(/(:<remoteRev>[\w-]+)[.]+(:<localRev>[\w-]+)\s+(:<remoteBranch>[\w-]+)\s+->\s+(:<localBranch>[\w-]+)/,
-              'pushed\nlocal\tlocalBranch@localRev\nremote\tremoteBranch@remoteRev');
-            this.success('Everything up-to-date');
-            sendGhAuth(this);
-            this.display();
-
-          }).skip(!machine.profileData.github).then(function(then){
-            var tagname = this.getValue('newRevision');
-            var releaseType = this.getValue('releaseType');
-            var branch = this.getValue('branch');
-            gitHubRelease(this, branch, pkg.name, tagname, releaseType, then);
-
-          }).title('', '\nAll done !\n\n' +
-          'Published <%=pkgName%>\n' +
-          'on <%=branch%> to <%=releaseType%> <%=newRevision%>\n')
-
-          .run(new Transport());
-
-      });
-  });
-
-program.parse(process.argv);
