050ceb55a550d14b1f971849cbed5ab6d8bf170f
clement <maboiteaspam@wanadoo.fr>
2015-06-20 17:55:32 +0800

    Add shortReleaseLog to commit the repo when it is dirty.,```

--- index.js
+++ index.js
@@ -94,8 +94,14 @@ var env = !program.env?'local':program.env;
         this.saveValue('releaseCommits', stdout);
       }).generateTemplate('<%=releaseLogTpl%>', '<%=tmpReleaseLog%>', {releaseCommits:'<%=releaseCommits%>'}, function(){
       }).textedit('Write the release log', '<%=tmpReleaseLog%>', function(changelog){
-        this.saveValue('releaseLog', changelog+'\n');
-      }).stream('git commit -am <%=quote("releaseLog")%>', function(){
+        this.saveValue('releaseLog', changelog);
+        var shortReleaseLog = changelog.toString().split('\n');
+        shortReleaseLog.shift();
+        shortReleaseLog.shift();
+        shortReleaseLog.shift();
+        shortReleaseLog.pop();
+        this.saveValue('shortReleaseLog', shortReleaseLog);
+      }).stream('git commit -am <%=quote("shortReleaseLog")%>', function(){
         this.success(/\[([\w-]+)\s+([\w-]+)]/i,
           'branch\t\t%s\nnew revision\t%s');
         this.success(/([0-9]+)\s+file[^0-9]+?([0-9]+)?[^0-9]+?([0-9]+)?/i,
